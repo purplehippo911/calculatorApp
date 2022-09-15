939211a3b23bb0f619c5d65c1ef950e36cf11afb
Omer Abdulshakur Abadula <omaba004@osloskolen.no>
2022-09-15 12:58:08 +0200

    🔨🎨 finished with styling the different themes

--- main.js
+++ main.js
@@ -13,16 +13,22 @@ methods: {
     if(this.i == 2 )  {
         console.log("its at column 2")
         this.$refs.slider.style.gridColumnStart = "2";
-        this.document.body.classList.add("dark")
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
     }
     if(this.i == 4 )  {
-        console.log("its at column 3")
+        console.log("its at column 3");
         this.$refs.slider.style.gridColumnStart = "3";
+        document.body.classList.remove("light");
+        document.body.classList.add("neon");
     }
     if(this.i > 5 )  {
-        console.log("its back to column 1")
-        this.$refs.slider.style.gridColumnStart = "1";
         this.i = 0;
+        console.log("its back to column 1");
+        this.$refs.slider.style.gridColumnStart = "1";
+        document.body.classList.remove("neon");
+        document.body.classList.add("dark");
+
     }
 
     }
