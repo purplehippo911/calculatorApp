abd25983e7ccf6f10073c58a40690700424a16d9
redfox <76628594+purplehippo911@users.noreply.github.com>
2022-09-17 12:42:44 +0200

    vue is working and is ready to pull the changes from the repository

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-
-const app = new Vue({
-el:'#app',
-data() {
-    return {
-        mesage: "Hello Vue!"
+const app = Vue.createApp({
+    data() {
+      return {
+            message: "Hello Vue!",
+            i: 0
+      }
+    },
+    methods: {
+        toggleOpen() {
+            if(this.i = 1) {
+                this.$refs.slider.style.left = `20px`;
+            }
+            if(this.i = 2) {
+                this.$refs.slider.style.left = `20px`;
+            }
+            if(this.i > 3 ) {
+                this.i = 0;
+            }
+            this.i = this.i + 1 ;
+            console.log(this.i);
+        }
     }
-}        
-    
 })
+
+app.mount("#app");
